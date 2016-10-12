7b3dc58ef2bdb55f9cd3b204a88b3ec213d5a42c
Nikita <tyumencev2004@bk.ru>
2016-10-12 20:22:06 +0300

    Some fixes

--- com/moofMonkey/audio/Wav.java
+++ com/moofMonkey/audio/Wav.java
@@ -1,15 +1,13 @@
 package com.moofMonkey.audio;
 
 
-import java.applet.Applet;
-import java.applet.AudioClip;
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
+import java.io.Closeable;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 import javax.sound.sampled.AudioFormat;
@@ -17,7 +15,6 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
 import javax.sound.sampled.SourceDataLine;
-import javax.sound.sampled.TargetDataLine;
 
 import com.moofMonkey.utils.IOUtils;
 
@@ -26,17 +23,13 @@ import com.moofMonkey.utils.IOUtils;
  * @author moofMonkey
  */
 public class Wav {
-	ByteArrayOutputStream byteArrayOutputStream;
-	TargetDataLine targetDataLine;
-	AudioInputStream audioInputStream;
-	SourceDataLine sourceDataLine;
 	float speed = 1F;
 	private String filePath;
 	/**
 	 * Must be "RIFF"
 	 */
 	private String chunkID;
-	private int chunkSize = 0;
+	public int chunkSize = 0;
 	/**
 	 * Must be "WAVE"
 	 */
@@ -45,16 +38,16 @@ public class Wav {
 	 * Must be "fmt*"
 	 */
 	private String subChunk1ID;
-	private int subChunk1Size = 0;
-	private short audioFormat = 0;
-	private short numChannels = 0;;
-	private int sampleRate = 0;
-	private int byteRate = 0;
-	private short blockAlign = 0;
-	private short bitsPerSample = 0;
-	private String specialData = "";
+	public int subChunk1Size = 0;
+	public short audioFormat = 0;
+	public short numChannels = 0;;
+	public int sampleRate = 0;
+	public int byteRate = 0;
+	public short blockAlign = 0;
+	public short bitsPerSample = 0;
+	public String specialData = "";
 	private String subChunk2ID = "data";
-	public byte[] soundData;
+	public byte[] audioData;
 	public byte[] dataAtEnd;
 
 	public Wav() {
@@ -96,8 +89,8 @@ public class Wav {
 			specialData = specialData.substring(0, specialData.length() - subChunk2ID.length());
 
 			int subChunk2Size = io.readInt();
-			soundData = new byte[(int) subChunk2Size];
-			io.getIn().readFully(soundData);
+			audioData = new byte[(int) subChunk2Size];
+			io.getIn().readFully(audioData);
 			
 			dataAtEnd = new byte[io.getIn().available()];
 			io.getIn().readFully(dataAtEnd);
@@ -127,8 +120,8 @@ public class Wav {
 			io.writeShort(bitsPerSample);
 			io.writeString(specialData);
 			io.writeString(subChunk2ID); //data
-			io.writeInt(soundData.length);
-			io.getOut().write(soundData);
+			io.writeInt(audioData.length);
+			io.getOut().write(audioData);
 			//outFile.write(dataAtEnd);
 		} catch(Throwable t) {
 			t.printStackTrace();
@@ -137,20 +130,53 @@ public class Wav {
 
 		return true;
 	}
+	
+	public String getDuration() {
+		float duration = speed * audioData.length / (bitsPerSample / Byte.SIZE) / numChannels / sampleRate;
+		int durationSec = (int) (duration % 60);
+		int durationMin = (int) (duration / 60);
+		int durationHour = (int) (duration - (durationMin * 60 + durationSec));
+
+		String strDurationMilliSec = Integer.toString (
+			(int) (
+				(duration % 1)
+				* 1000
+			)
+		);
+		String strDurationSec = Integer.toString(durationSec);
+		String strDurationMin = Integer.toString(durationMin);
+		String strDurationHour = Integer.toString(durationHour);
+
+		while(strDurationMilliSec.length() < 4)
+			strDurationMilliSec = "0" + strDurationMilliSec;
+		while(strDurationSec.length() < 2)
+			strDurationSec = "0" + strDurationSec;
+		while(strDurationMin.length() < 2)
+			strDurationMin = "0" + strDurationMin;
+		while(strDurationHour.length() < 2)
+			strDurationHour = "0" + strDurationHour;
+		
+		return strDurationHour + ":" + strDurationMin + ":" + strDurationSec + ":" + strDurationMilliSec;
+	}
 
 	public String getSummary() {
 		String newline = System.getProperty("line.separator");
-		String summary = "Format: " + audioFormat + newline + "Channels: " + numChannels
-				+ newline + "Sample rate: " + sampleRate + newline + "Byte rate: "
-				+ byteRate + newline + "BlockAlign: " + blockAlign + newline
-				+ "Bits/1 Sample: " + bitsPerSample + newline + "Data size: "
-				+ soundData.length + newline + "Data at end: " + new String(dataAtEnd);
+		String summary =
+				"Format: " + audioFormat + newline +
+				"Channels: " + numChannels + newline +
+				"Sample rate: " + sampleRate + newline +
+				"Byte rate: " + byteRate + newline +
+				"BlockAlign: " + blockAlign + newline +
+				"Bits/1 Sample: " + bitsPerSample + newline +
+				"Data size: " + audioData.length + newline +
+				"Data at end: " + new String(dataAtEnd) + newline +
+				"Duration: " + getDuration();
 		
 		return summary;
 	}
 
 	public byte[] getBytes() throws Throwable {
-		return soundData;
+		return audioData;
 	}
 
 	private AudioFormat getAudioFormat() {
@@ -159,24 +185,32 @@ public class Wav {
 
 		return new AudioFormat(sampleRate * speed, bitsPerSample, numChannels, signed, bigEndian);
 	}
-
-	public void playWav(String filePath) {
-		try {
-			AudioClip clip = (AudioClip) Applet.newAudioClip(new File(filePath).toURI().toURL());
-			clip.play();
-		} catch(Throwable t) {
-			t.printStackTrace();
-		}
-	}
 	
 	/**
 	 * Plays back audio stored in the byte array using an audio format given by
 	 * freq, sample rate, etc.
 	 */
+	@SuppressWarnings( "resource" )
 	public void playAudio() {
 		try {
-			byte[] audioData = soundData;
-			
+			new PlayThread(audioData).start();
+		} catch(Throwable t) {
+			t.printStackTrace();
+		}
+	}
+
+	public static PlayThread currentPlaying;
+	/**
+	 * Inner class to play back the data that was saved
+	 */
+	private class PlayThread extends Thread implements Closeable {
+		AudioInputStream audioInputStream;
+		SourceDataLine sourceDataLine;
+		byte[] tempBuffer = new byte[10000];
+		
+		public PlayThread(byte[] audioData) throws Throwable {
+			if(currentPlaying != null)
+				currentPlaying.close();
 			InputStream byteArrayInputStream = new ByteArrayInputStream(audioData);
 			AudioFormat audioFormat = getAudioFormat();
 			audioInputStream = new AudioInputStream(
@@ -189,18 +223,8 @@ public class Wav {
 			sourceDataLine = (SourceDataLine) AudioSystem.getLine(dataLineInfo);
 			sourceDataLine.open(audioFormat);
 			sourceDataLine.start();
-
-			new PlayThread().start();
-		} catch(Throwable t) {
-			t.printStackTrace();
+			currentPlaying = this;
 		}
-	}
-
-	/**
-	 * Inner class to play back the data that was saved
-	 */
-	private class PlayThread extends Thread {
-		byte[] tempBuffer = new byte[10000];
 
 		public void run() {
 			try {
@@ -209,11 +233,17 @@ public class Wav {
 					if (cnt > 0)
 						sourceDataLine.write(tempBuffer, 0, cnt); // Write data to the internal buffer of the data line where it will be delivered to the speaker.
 				sourceDataLine.drain();
-				sourceDataLine.close();
-			} catch(Exception e) {
-				System.out.println(e);
-				System.exit(0);
+				close();
+			} catch(Throwable t) {
+				t.printStackTrace();
 			}
+		}
+
+		@Override
+		public void close() throws IOException {
+			currentPlaying = null;
+			interrupt();
+			sourceDataLine.close();
 		}
 	}
 }
